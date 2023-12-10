c69e3db2fb5e35f62697e3cc66356c786065c485
GURNEET SINGH <79376134+SinghGurneet21@users.noreply.github.com>
2023-12-11 02:13:17 +0530

    Add files via upload

--- templates/python_task_1.py
+++ templates/python_task_1.py
@@ -12,15 +12,7 @@ def generate_car_matrix(df)->pd.DataFrame:
         pandas.DataFrame: Matrix generated with 'car' values, 
                           where 'id_1' and 'id_2' are used as indices and columns respectively.
     """
-    # Write your logic her
-    # Pivot the DataFrame to create a matrix
-    car_matrix = df.pivot(index='id_1', columns='id_2', values='car')
-
-    # Fill NaN values with 0
-    df = car_matrix.fillna(0)
-
-    # Set diagonal values to 0
-    df.values[[range(len(car_matrix))]*2] = 0
+    # Write your logic here
 
     return df
 
@@ -35,20 +27,9 @@ def get_type_count(df)->dict:
     Returns:
         dict: A dictionary with car types as keys and their counts as values.
     """
-    # Add a new categorical column 'car_type'
-    conditions = [
-        (df['car'] <= 15),
-        (df['car'] > 15) & (df['car'] <= 25),
-        (df['car'] > 25)
-    ]
-    choices = ['low', 'medium', 'high']
-    df['car_type'] = pd.cut(df['car'], bins=[float('-inf'), 15, 25, float('inf')], labels=choices, right=False)
+    # Write your logic here
 
-    # Calculate the count of occurrences for each 'car_type' category
-    type_count = df['car_type'].value_counts().to_dict()
-
-    # Sort the dictionary alphabetically based on keys
-    return dict(sorted(type_count.items()))
+    return dict()
 
 
 def get_bus_indexes(df)->list:
@@ -62,12 +43,8 @@ def get_bus_indexes(df)->list:
         list: List of indexes where 'bus' values exceed twice the mean.
     """
     # Write your logic here
-    # Calculate the mean value of the 'bus' column
-    mean_bus_value = df['bus'].mean()
 
-    # Identify indices where 'bus' values are greater than twice the mean
-    bus_indexes = df[df['bus'] > 2 * mean_bus_value].index
-    return sorted(list(bus_indexes))
+    return list()
 
 
 def filter_routes(df)->list:
@@ -81,12 +58,8 @@ def filter_routes(df)->list:
         list: List of route names with average 'truck' values greater than 7.
     """
     # Write your logic here
-    # Group by 'route' and calculate the average of the 'truck' column
-    average_truck_by_route = df.groupby('route')['truck'].mean()
 
-    # Filter routes where the average of 'truck' column is greater than 7
-    selected_routes = average_truck_by_route[average_truck_by_route > 7].index
-    return sorted(list(selected_routes))
+    return list()
 
 
 def multiply_matrix(matrix)->pd.DataFrame:
@@ -100,11 +73,8 @@ def multiply_matrix(matrix)->pd.DataFrame:
         pandas.DataFrame: Modified matrix with values multiplied based on custom conditions.
     """
     # Write your logic here
-    modified_matrix = input_matrix.applymap(lambda x: x * 0.75 if x > 20 else x * 1.25)
 
-    # Round the values to 1 decimal place
-    modified_matrix_rounded = modified_matrix.round(1)
-    return modified_matrix_rounded
+    return matrix
 
 
 def time_check(df)->pd.Series:
@@ -118,26 +88,5 @@ def time_check(df)->pd.Series:
         pd.Series: return a boolean series
     """
     # Write your logic here
-        # Define a mapping of day names to their corresponding order
-    day_order = {'Monday': 0, 'Tuesday': 1, 'Wednesday': 2, 'Thursday': 3, 'Friday': 4, 'Saturday': 5, 'Sunday': 6}
 
-    # Convert day names to their order in the week
-    data['start_order'] = data['startDay'].map(day_order)
-    data['end_order'] = data['endDay'].map(day_order)
-
-    # Calculate the absolute difference considering the circular nature of days
-    data['circular_difference'] = (data['end_order'] - data['start_order'] + 7) % 7
-
-    # Check if there is a time difference between start and end days
-    result = data[data['circular_difference'] >0]
-
-    # Group by 'id' and 'id2' and return the result
-    grouped_result = result.groupby(['id', 'id_2']).agg({
-        'startDay': 'first',
-        'endDay': 'first',
-        'startTime': 'first',
-        'endTime': 'first',
-        'circular_difference': 'first'
-    }).reset_index()
-
-    return pd.Series(list(grouped_result.sort_values(by='circular_difference', ascending=False).index))
+    return pd.Series()
